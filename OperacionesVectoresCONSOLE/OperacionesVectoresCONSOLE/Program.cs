e88ec3ad71bc0b081d7902e98a94342dc926e7a1
digobat17 <diegobat13@gmail.com>
2019-05-22 08:26:35 -0400

    TareaVectores

---
+++ OperacionesVectoresCONSOLE/OperacionesVectoresCONSOLE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesVectoresCONSOLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v = new Vector();
+            Console.WriteLine("Suma de dos vectores");
+            v.cargar();
+            v.Sumar();
+            v.Restar();
+             
+            
+        }
+    }
+}
